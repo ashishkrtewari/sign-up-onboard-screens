b5b1982a4c3d0e6fbedfc796b92391bcfa781ccb
Ashish Kumar Tewari <ashishtewaripro@gmail.com>
2021-01-24 22:52:51 +0530

    Implemented SignIn to SignUp switchState

--- app.js
+++ app.js
@@ -14,6 +14,46 @@
       dialog.classList.add("opacity-0");
       dialog.innerHTML = null;
     },
+    switchState: () => {
+      const form = [].slice.call(document.getElementsByTagName('form'))[0];
+      const formHeadline = form.querySelector('.form-headline');
+      const type = formHeadline?.dataset?.target;
+      const forgotPassword = form.querySelector('.forgot-password');
+      const actionButton = form.querySelector('.action-button');
+      const alreadyAccount = form.querySelector('.already-account');
+      const signInLink = form.querySelector('.sign-in-link');
+      const labels = {
+        formHeadline: {
+          signIn: 'Sign In',
+          signUp: 'Sign Up'
+        },
+        actionButton: {
+          signIn: 'Login',
+          signUp: 'Create Account'
+        },
+        alreadyAccount: {
+          signIn: 'Create a new account.',
+          signUp: 'Already have an account?'
+        },
+        signInLink: {
+          signIn: 'Sign Up',
+          signUp: 'Sign In'
+        }
+      }
+
+      formHeadline.innerHTML = labels.formHeadline[type];
+      actionButton.innerHTML = labels.actionButton[type];
+      alreadyAccount.innerHTML = labels.alreadyAccount[type];
+      signInLink.innerHTML = labels.signInLink[type];
+
+      if (type === 'signUp') {
+        formHeadline.dataset.target = 'signIn'; 
+        forgotPassword.classList.add('opacity-0');
+      } else {
+        formHeadline.dataset.target = 'signUp';
+        forgotPassword.classList.remove('opacity-0');
+      }
+    }
   };
   document.addEventListener("DOMContentLoaded", () => {
     const pageTargets = [].slice.call(
